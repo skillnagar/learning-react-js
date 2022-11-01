acfb765f53686b2c876c9ff443352e5e5ece5421
Avinash Seth <bloggerbeg@gmail.com>
2022-11-01 11:55:32 +0530

    checkbox checked using useState

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  const [emotion, setEmotion] = useState("happy");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`);
-  }, [emotion]);
+  const [checked, setChecked] = useState(false);
 
   return (
     <div className="App">
-      <h1>Current emotion is { emotion }</h1>
-      <button onClick={ () => setEmotion("sad") }>Sad</button>
-      <button onClick={ () => setEmotion("excited") }>Excited</button>
-      <button onClick={ () => setEmotion("happy") }>Default</button>
+      <input type="checkbox" value={checked} onChange={ () => setChecked((checked) => !checked)}/>
+      <label>{ checked ? "checked" : "not checked" }</label>
     </div>
   );
 }
