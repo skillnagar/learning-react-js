5ea096c376f3971f6b827af53bcfc7892eab5c70
Avinash Seth <bloggerbeg@gmail.com>
2022-11-01 12:03:50 +0530

    working with useRef

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import { useReducer } from 'react';
+import { useRef } from 'react';
 
 function App() {
 
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  const txtTitle = useRef();
+  const hexColor = useRef();
+
+  const submit = e => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`);
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  };
 
   return (
-    <div className="App">
-      <input type="checkbox" value={checked} onChange={setChecked} />
-      <label>{ checked ? "checked" : "not checked" }</label>
-    </div>
+
+    <form onSubmit={submit}>
+      <input ref={txtTitle} type="text" placeholder="color title..." />
+      <input ref={hexColor} type="color" />
+      <button>Add</button>
+    </form>
+
   );
 }
 
